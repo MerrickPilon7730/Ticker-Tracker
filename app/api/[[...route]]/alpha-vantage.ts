83e42cc5f7ba8cbaa8a5e4faf555f85729e73b98
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-11 14:01:20 -0400

    added routes for hono and added a get top gainers and loseres api call

---
+++ app/api/[[...route]]/alpha-vantage.ts
@@ -0,0 +1,15 @@
+
+import { Hono } from "hono";
+
+const API_KEY = process.env.ALPHA_VANTAGE_API_KEY;
+
+const app = new Hono()
+    .get("/gainers-losers", async (c) => {
+        const response = await fetch(`https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=${API_KEY}`)
+
+        const data = await response.json();
+
+        return c.json(data);
+    })
+
+export default app;
---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,17 @@
+
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+import alphaVantage from "./alpha-vantage"
+
+export const runtime = "edge"
+
+const app = new Hono().basePath("/api")
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes = app
+    .route("/alpha-vantage", alphaVantage)
+
+export const GET = handle(app)
+
+export type AppType = typeof routes; 
