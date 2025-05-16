12e44715fcd236c9515ff1a2a84a84b1bd6e75aa
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-16 15:50:14 -0400

    Added validation at runtime for top gainers/losers/most traded.

--- features/search/get-top-gainers-losers.ts
+++ features/search/get-top-gainers-losers.ts
@@ -1,47 +1,61 @@
 
-type Props = {
-    symbol: string,
-    price: string,
-    change_amount: string,
-    change_percentage: string,
-}
+import {z} from "zod";
 
-export const getTopGainers = async (): Promise<Props[]> => {
+const StockData = z.object({
+    symbol: z.string(),
+    price: z.string(),
+    change_amount: z.string(),
+    change_percentage: z.string(),
+    volume: z.string(),
+});
+
+const GainersLosersData = z.object({
+    metadata: z.string(),
+    last_updated: z.string(),
+    top_gainers: z.array(StockData),
+    top_losers: z.array(StockData),
+    most_actively_traded: z.array(StockData),
+});
+
+export const getTopGainersLosers = async (): Promise<z.infer<typeof GainersLosersData>> => {
     const response = await fetch("http://localhost:3000/api/alpha-vantage/gainers-losers");
 
     if (!response.ok) {
         throw new Error("Failed to fetch data");
     }
 
-    const data = await response.json();
+    const json = await response.json();
+    const data = GainersLosersData.parse(json);
     
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const gainers: Props[] = data.top_gainers.map((item: any) => ({
-        symbol: item.ticker,
-        price: item.price,
-        change_amount: item.change_amount,
-        change_percentage: item.change_percentage,
-    }));
-
-    return gainers;
+    return data;
 }
 
-export const getTopLosers = async (): Promise<Props[]> => {
-    const response = await fetch("http://localhost:3000/api/alpha-vantage/gainers-losers");
+export const getTopFourGainers = async (): Promise<z.infer<typeof StockData>[]> => {
+    const topFourGainers = await getTopGainersLosers();
+    return topFourGainers.top_gainers.slice(0,4);
+}
 
-    if (!response.ok) {
-        throw new Error("Failed to fetch data");
-    }
+export const getTopFourLosers = async (): Promise<z.infer<typeof StockData>[]> => {
+    const topFourLosers = await getTopGainersLosers();
+    return topFourLosers.top_losers.slice(0,4);
+}
 
-    const data = await response.json();
-    
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const losers: Props[] = data.top_losers.map((item: any) => ({
-        symbol: item.ticker,
-        price: item.price,
-        change_amount: item.change_amount,
-        change_percentage: item.change_percentage,
-    }));
+export const getTopFourTraded = async (): Promise<z.infer<typeof StockData>[]> => {
+    const topFourTraded = await getTopGainersLosers();
+    return topFourTraded.most_actively_traded.slice(0,4);
+}
 
-    return losers;
+export const getAllGainers = async (): Promise<z.infer<typeof StockData>[]> => {
+    const topGainers = await getTopGainersLosers();
+    return topGainers.top_gainers;
+}
+
+export const getAllLosers = async ():Promise<z.infer<typeof StockData>[]> => {
+    const topLosers = await getTopGainersLosers();
+    return topLosers.top_losers;
+}
+
+export const getMostActivelyTraded = async (): Promise<z.infer<typeof StockData>[]> => {
+    const mostTraded = await getTopGainersLosers();
+    return mostTraded.most_actively_traded;
 }
