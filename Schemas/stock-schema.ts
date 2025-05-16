951c4673b332541848a21bf78a0e8b8ef201e53f
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-16 16:42:00 -0400

    changed schema to be more consistent

--- Schemas/stock-schema.ts
+++ Schemas/stock-schema.ts
@@ -2,7 +2,7 @@
 import { z } from "zod";
 
 export const StockData = z.object({
-  symbol: z.string(),
+  ticker: z.string(),
   price: z.string(),
   change_amount: z.string(),
   change_percentage: z.string(),
