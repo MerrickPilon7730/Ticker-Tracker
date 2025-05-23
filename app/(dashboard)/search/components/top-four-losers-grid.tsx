7fcc9335b2b38af1b41e970b927d6765cbf71a6e
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-23 15:31:52 -0400

    changed the names to be more accurate

---
+++ app/(dashboard)/search/components/top-four-losers-grid.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+import { getTopFourLosers } from "@/features/search/get-top-gainers-losers";
+
+import { StockModal } from "@/components/stock-modal";
+
+import { GainersLosersCard } from "./gainers-losers-card";
+
+import { StockDataType } from "@/Schemas/stock-schema";
+
+export const TopFourLosersGrid = () => {
+  const [losers, setLosers] = useState<StockDataType[]>([]);
+  const [selectedStock, setSelectedStock] = useState<StockDataType | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    getTopFourLosers().then(setLosers);
+  }, []);
+
+    const handleCardClick = (stock: StockDataType) => {
+    setSelectedStock(stock);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-4 w-full auto-rows-min">
+        {losers.map((stock) => (
+          <GainersLosersCard
+            key={stock.ticker}
+            title={stock.ticker}
+            stock={stock}
+            onClick={() => handleCardClick(stock)}
+          />
+        ))}
+      </div>
+      <StockModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        stock={selectedStock}
+      />
+    </>
+  );
+};
