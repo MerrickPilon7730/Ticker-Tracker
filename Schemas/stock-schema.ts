bdfbdfac26304479a13259c23ccd76277e1b8657
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-23 14:26:34 -0400

    added more schemas for API calls and made them global so I don't have to keep redefining them

--- Schemas/stock-schema.ts
+++ Schemas/stock-schema.ts
@@ -1,6 +1,21 @@
 
 import { z } from "zod";
 
+export const Stock = z.object({
+  symbol: z.string(),
+  name: z.string(),
+  currency: z.string(),
+  exchange: z.string(),
+});
+
+export type StockType = z.infer<typeof Stock>;
+
+export const StockPrice = z.object({
+  price: z.string(),
+});
+
+export type StockPriceType = z.infer<typeof StockPrice>;
+
 export const StockData = z.object({
   ticker: z.string(),
   price: z.string(),
@@ -9,3 +24,13 @@ export const StockData = z.object({
 });
 
 export type StockDataType = z.infer<typeof StockData>;
+
+export const GainersLosersData = z.object({
+    metadata: z.string(),
+    last_updated: z.string(),
+    top_gainers: z.array(StockData),
+    top_losers: z.array(StockData),
+    most_actively_traded: z.array(StockData),
+});
+
+export type GainersLosersDataType = z.infer<typeof GainersLosersData>;
