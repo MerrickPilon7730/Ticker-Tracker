d157950ce84daee84d98ed7b2e2f2ea9492ba455
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-23 16:17:35 -0400

    Created all losers/most traded grids

---
+++ app/(dashboard)/search/components/all-most-traded.grid.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+import { getMostActivelyTraded } from "@/features/search/get-top-gainers-losers";
+
+import { StockModal } from "@/components/stock-modal";
+
+import { GainersLosersCard } from "./gainers-losers-card";
+
+import { StockDataType } from "@/Schemas/stock-schema";
+
+export const TopTradedGrid = () => {
+  const [traded, setTraded] = useState<StockDataType[]>([]);
+   const [selectedStock, setSelectedStock] = useState<StockDataType | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    getMostActivelyTraded().then(setTraded);
+  }, []);
+
+  const handleCardClick = (stock: StockDataType) => {
+    setSelectedStock(stock);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-4 w-full auto-rows-min">
+        {traded.map((stock) => (
+          <GainersLosersCard
+            key={stock.ticker}
+            title={stock.ticker}
+            stock={stock}
+            onClick={() => handleCardClick(stock)}
+          />
+        ))}
+      </div>
+      <StockModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        stock={selectedStock}
+      />
+    </>
+  );
+};
