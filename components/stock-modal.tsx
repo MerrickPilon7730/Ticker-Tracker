ac2c2d94c85c43aa4160894986e09901537f5270
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-24 11:04:44 -0400

    Edited the stock-modal to pass/use the information from stockQuote

--- components/stock-modal.tsx
+++ components/stock-modal.tsx
@@ -1,42 +1,51 @@
-
 import { 
-    Card, 
-    CardContent, 
-    CardHeader, 
-    CardTitle 
+  Card, 
+  CardContent, 
+  CardHeader, 
+  CardTitle 
 } from "@/components/ui/card";
-import { StockDataType } from "@/Schemas/api-schema";
+
+import { StockDataType, StockQuoteType } from "@/Schemas/api-schema";
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
-  stock: StockDataType | null,
+  stock: StockDataType | null;
+  stockQuote: StockQuoteType | null;
 };
 
-export const StockModal = ({ isOpen, onClose, stock }: Props) => {
+export const StockModal = ({ isOpen, onClose, stock, stockQuote }: Props) => {
   if (!isOpen || !stock) return null;
   // TODO: fix layout of modal, make it bigger, get more data from API
   return (
-    <div 
-  className="fixed inset-0 z-50 bg-black/60 flex items-center justify-center"
-  onClick={onClose}
->
-  <div 
-    className="w-[90%] max-w-4xl" 
-    onClick={(e) => e.stopPropagation()}
-  >
-    <Card className="bg-gradient-to-b from-emerald-500 to-slate-600 p-4 border-none">
-      <CardHeader className="flex justify-between items-center">
-        <CardTitle>{stock.ticker} Details</CardTitle>
-      </CardHeader>
-      <CardContent className="space-y-2 max-h-[70vh] overflow-y-auto">
-        <p><strong>Price:</strong> ${Number(stock.price).toFixed(2)}</p>
-        <p><strong>Change:</strong> ${Number(stock.change_amount).toFixed(2)}</p>
-        <p><strong>Change %:</strong> {stock.change_percentage}</p>
-      </CardContent>
-    </Card>
-  </div>
-</div>
-
+    <div
+      className="fixed inset-0 z-50 bg-black/60 flex items-center justify-center"
+      onClick={onClose}
+    >
+      <div 
+        className="w-[90%] max-w-4xl" 
+        onClick={(e) => e.stopPropagation()}
+        >
+        <Card className="bg-gradient-to-b from-emerald-500 to-slate-600 p-4 border-none">
+          <CardHeader className="flex justify-between items-center">
+            <CardTitle>{stock.ticker} Details</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-2 max-h-[70vh] overflow-y-auto">
+            <p>
+              <strong>Price:</strong> ${Number(stock.price).toFixed(2)}
+            </p>
+            <p>
+              <strong>Change:</strong> ${Number(stock.change_amount).toFixed(2)}
+            </p>
+            <p>
+              <strong>Change %:</strong> {stock.change_percentage}
+            </p>
+            <p>
+              {stockQuote?.exchange}
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 };
