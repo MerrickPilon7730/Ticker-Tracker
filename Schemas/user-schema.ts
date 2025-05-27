f599bb8e6c0d68567c9a23b7b589a404650b484e
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-26 21:09:26 -0400

    Created schema for DynamoDB

---
+++ Schemas/user-schema.ts
@@ -0,0 +1,17 @@
+
+import { z } from "zod";
+
+export const ownedStocks = z.object({
+    quantity: z.number().int().nonnegative(),
+    price: z.number().nonnegative(),
+});
+
+export const ownedStocksSchema = z.record(z.string(), z.array(ownedStocks));
+
+export const userData = z.object({
+    userId: z.string(),
+    email: z.string(),
+    password: z.string(),
+    watchList: z.array(z.string()),
+    owned: ownedStocksSchema,
+});
