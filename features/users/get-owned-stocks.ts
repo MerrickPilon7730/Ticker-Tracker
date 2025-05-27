1bbf0fd71e9e16548faa2874f6ca8c2e70be8a8f
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-27 15:00:34 -0400

    Added utility function to fetch data from database api route

---
+++ features/users/get-owned-stocks.ts
@@ -0,0 +1,20 @@
+
+import { ownedStocksSchema, ownedStocksType } from "@/Schemas/user-schema";
+
+export const getUserOwnedStocks = async (userId: string): Promise<ownedStocksType> => {
+  const response = await fetch(`http://localhost:3000/api/dynamodb/owned/${userId}`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch user owned stocks");
+  }
+
+  const data = await response.json();
+
+  const parsedData = ownedStocksSchema.safeParse(data.owned);
+
+  if (!parsedData.success) {
+    throw new Error("Invalid response format");
+  }
+
+  return parsedData.data;
+};
