358cc38bb74c001db3142e6f295258a4557dbbd3
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-05-30 12:17:23 -0400

    Passed the secret key so it is available at build time

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
-
-console.log('Running in Edge?', typeof process.env.CLERK_SECRET_KEY === 'undefined');
-console.log('CLERK_SECRET_KEY:', process.env.CLERK_SECRET_KEY);
+export default clerkMiddleware({
+  secretKey: process.env.CLERK_SECRET_KEY,
+})
 
 export const config = {
   matcher: [
