24c148c1af3eed2c1e334aff4efa68ad03ebabb3
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-05 09:18:44 -0400

    added publishable key to clerkprovider

--- app/(dashboard)/overview/page.tsx
+++ app/(dashboard)/overview/page.tsx
@@ -1,9 +1,13 @@
 
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function OverviewPage() {
+export default async function OverviewPage() {
+    const { userId } = await auth();
+    const user = await currentUser();
+
     return (
         <div>
-            Overview
+            Welcome, {user?.firstName}, {userId}
         </div>
     );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
  return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html>
         <body className="bg-slate-600 text-white">
           <NavbarWrapper />
