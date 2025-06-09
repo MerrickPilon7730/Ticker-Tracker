da0c212638f9b1b72bbaa547006ca1a83184ad3f
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-09 10:00:03 -0400

    added get request for daily time series

--- app/api/[[...route]]/alpha-vantage.ts
+++ app/api/[[...route]]/alpha-vantage.ts
@@ -1,3 +1,4 @@
+
 import { Hono } from "hono";
 
 const API_KEY = process.env.ALPHA_VANTAGE_API_KEY
@@ -8,6 +9,18 @@ const app = new Hono()
 
         if(!response.ok){
             return c.json({error: "Unable to fetch stocks"}, 400);
+        }
+
+        const data = await response.json();
+
+        return c.json(data);
+    })
+    .get("/stocks/:symbol/daily-time-series", async (c) =>{
+        const symbol = c.req.param("symbol");
+        const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${API_KEY}`)
+
+        if (!response.ok) {
+            return c.json({error: "Failed to fetch daily time series"}, 400);
         }
 
         const data = await response.json();
